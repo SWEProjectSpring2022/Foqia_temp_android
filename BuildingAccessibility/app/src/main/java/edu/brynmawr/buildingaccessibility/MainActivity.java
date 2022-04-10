73a998312a35f6f76c9ff21c1e35f98cff385ab9
Foqia Shahid <fshahid@brynmawr.edu>
2022-04-10 18:41:22 -0400

    adding initial files

---
+++ BuildingAccessibility/app/src/main/java/edu/brynmawr/buildingaccessibility/MainActivity.java
@@ -0,0 +1,38 @@
+package edu.brynmawr.buildingaccessibility;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int VIEW_ACTIVITY_ID = 1;
+    private static final int SEARCH_ACTIVITY_ID = 2;
+    private static final int POST_ACTIVITY_ID = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onViewButtonClick(View v){
+        Intent i = new Intent(this, ViewActivity.class);
+        i.putExtra("MESSAGE", "Going to view all buildings");
+        startActivityForResult(i,VIEW_ACTIVITY_ID);
+    }
+
+    public void onSearchButtonClick(View view) {
+        Intent i = new Intent(this, SearchActivity.class);
+        i.putExtra("MESSAGE", "Going to search a building");
+        startActivityForResult(i,SEARCH_ACTIVITY_ID);
+    }
+
+    public void onPostButtonClick(View view) {
+        Intent i = new Intent(this, PostActivity.class);
+        i.putExtra("MESSAGE", "Going to post on the forum");
+        startActivityForResult(i,POST_ACTIVITY_ID);
+    }
+}
